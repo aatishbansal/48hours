2ff0d177f24c4518768f85c00c77465beeaff284
Aatish Bansal <aatish.bansal@ukg.com>
2023-01-16 13:26:09 +0530

    Commit

--- src/main/java/com/ukg/dao/LeaveRepository.java
+++ src/main/java/com/ukg/dao/LeaveRepository.java
@@ -14,4 +14,14 @@ public interface LeaveRepository extends JpaRepository<LeaveEntity, Long> {
 	
 	@Query(value = "select empid, sum(amount) as amount, 'QUARTERLY' as TYPE from employeewellbeing.leave where datesubmit > (CURRENT_DATE - INTERVAL '90 days') and  type != 'SICK_LEAVE' group by empid", nativeQuery = true)
 	List<LeaveEntity> findAllEmployeeFromLastQuarter();
+	
+	@Query(value = "select empid, sum(amount) as amount, 'MONTHLY' as TYPE from employeewellbeing.leave where datesubmit >= (CURRENT_DATE - INTERVAL '30 days') and  type != 'SICK_LEAVE' group by empid", nativeQuery = true)
+	List<LeaveEntity> findAllEmployeeFromMonthly();
+	
+	@Query(value = "select empid, sum(amount) as amount, 'BIMONTHLY' as TYPE from employeewellbeing.leave where datesubmit >= (CURRENT_DATE - INTERVAL '60 days') and datesubmit < (CURRENT_DATE - interval '30 days')  and  type != 'SICK_LEAVE' group by empid", nativeQuery = true)
+	List<LeaveEntity> findAllEmployeeFromBiMonthly();
+	
+	@Query(value = "select empid, sum(amount) as amount, 'QUARTERLY' as TYPE from employeewellbeing.leave where datesubmit >= (CURRENT_DATE - INTERVAL '90 days') and datesubmit < (CURRENT_DATE - interval '60 days')  and  type != 'SICK_LEAVE' group by empid", nativeQuery = true)
+	List<LeaveEntity> findAllEmployeeFromQuarterly();
+
 }
